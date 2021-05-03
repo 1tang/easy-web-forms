049d5ac8b7f23b3154a4c35199f92c72b8a27c4b
1tang <im_nicole@hotmail.co.uk>
2021-05-03 13:43:28 +0100

    Update for use
    Input on name, email address, flat number, and currently lives there?

--- dist/htdocs/example.php
+++ dist/htdocs/example.php
@@ -18,124 +18,20 @@ $email = new Input([
 ]);
 $address = new Input([
   'type' => 'address',
-  'label' => 'Delivery Address',
+  'label' => 'Flat number',
   'name' => 'address',
   'required' => true
 ]);
-$datetime = new Input([
-  'type' => 'datetime',
-  'label' => 'Delivery Time',
-  'name' => 'deliverytime',
-  'flatpickrOptions' => [
-    'altFormat' => 'h:i K',
-    'altInput' => true,
-    'defaultHour' => date('H') + 1,
-    'defaultMinute' => 00,
-    'enableTime' => true,
-    'minuteIncrement' => 5,
-    'noCalendar' =>true
-  ],
-  'required' => true
-]);
-$crust = new Select([
-  'name' => 'crust',
+
+$occupancy = new Select([
+  'name' => 'Do you live here as...',
   'options' => [
-    '' => 'Choose a crust',
-    'thin' => 'Thin & Crispy',
-    'deep' => 'Chicago Deep Dish',
-    'glutenfree' => 'Gluten Free'
+    '' => 'Choose an option',
+    'owner' => 'Owner',
+    'tenant' => 'Renter'
   ],
   'required' => true
 ]);
-$mozzarella = new Input([
-  'type' => 'radio',
-  'name' => 'cheese',
-  'id' => 'mozzarella',
-  'value' => 'mozzarella',
-  'checked' => true
-]);
-$soy = new Input([
-  'type' => 'radio',
-  'name' => 'cheese',
-  'id' => 'soy',
-  'value' => 'soy',
-  'label' => 'Soy (vegan)',
-  'checked' => false
-]);
-$sausage = new Input([
-  'type' => 'checkbox',
-  'name' => 'toppings',
-  'id' => 'sausage',
-  'value' => 'sausage'
-]);
-$pepperoni = new Input([
-  'type' => 'checkbox',
-  'name' => 'toppings',
-  'id' => 'pepperoni',
-  'value' => 'pepperoni'
-]);
-$peppers = new Input([
-  'type' => 'checkbox',
-  'name' => 'toppings',
-  'id' => 'peppers',
-  'value' => 'peppers',
-  'label' => 'Bell peppers'
-]);
-$tomato = new Input([
-  'type' => 'checkbox',
-  'name' => 'toppings',
-  'id' => 'tomato',
-  'value' => 'tomato'
-]);
-$onion = new Input([
-  'type' => 'checkbox',
-  'name' => 'toppings',
-  'id' => 'onion',
-  'value' => 'onion'
-]);
-$instructions = new Textarea([
-  'label' => 'Special Instructions',
-  'name' => 'instructions'
-]);
-
-// 3. Add the form controls to Form
-$form = new Form([
-  'adminEmail' => '',
-  'emailSubject' => 'Order form submitted by {{name}}',
-  'submitButtonText' => 'Order Now',
-  'successMsg' => 'Thanks for your order.'
-]);
-$form->addControl($name);
-// $form->addControl($email);
-// $form->addControl($address);
-// $form->addControl($datetime);
-// $form->addControl($crust);
-// $form->addGroup([
-//   'controls' => [
-//     $mozzarella,
-//     $soy
-//   ],
-//   'label' => 'Type of Cheese'
-// ]);
-// $form->addGroup([
-//   'arrangement' => 'stacked',
-//   'controls' => [
-//     $sausage,
-//     $pepperoni,
-//     $peppers,
-//     $tomato,
-//     $onion
-//   ]
-// ]);
-
-$image = new Input([
-  'type' => 'file',
-  'name' => 'image',
-  'required' => true
-]);
-
-$form->addControl($image);
-$form->addControl($instructions);
 
 ?>
 
